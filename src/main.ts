6a6995bb7874bc5fda0ae47b92f53156c3879b67
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2025-06-13 16:04:24 -0300

    Liberar cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*', // Example origin
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Example methods
+    }
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
